e88ae59e3bdaff76fe8daae2b1a2f4e44517c363
maxiaoqu <maxiaoqu@163.com>
2020-01-14 17:48:36 +0800

    新增方法类

---
+++ src/date/_libs/requiredArgs.ts
@@ -0,0 +1,7 @@
+export default function requiredArgs(required: number, args: IArguments) {
+  if (args.length < required) {
+    throw new TypeError(
+      required + ' argument' + (required > 1 ? 's' : '') + ' required, but only ' + args.length + ' present'
+    )
+  }
+}
