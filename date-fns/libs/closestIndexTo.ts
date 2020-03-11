d012b83e7612d3c0c88a872ce1baf35a0cec4004
maxiaoqu <maxiaoqu@163.com>
2020-03-11 11:37:47 +0800

    c \ com\close

---
+++ date-fns/libs/closestIndexTo.ts
@@ -0,0 +1,48 @@
+import toDate from "./toDate";
+import requiredArgs from "../utils/requiredArgs";
+
+export default function closestIndexTo(dirtyDateToCompare, dirtyDatesArray) {
+  requiredArgs(2, arguments)
+
+  var dateToCompare = toDate(dirtyDateToCompare)
+
+  if (isNaN(dateToCompare)) {
+    return NaN
+  }
+
+  var timeToCompare = dateToCompare.getTime()
+
+  var datesArray
+  // `dirtyDatesArray` is undefined or null
+  if (dirtyDatesArray == null) {
+    datesArray = []
+
+    // `dirtyDatesArray` is Array, Set or Map, or object with custom `forEach` method
+  } else if (typeof dirtyDatesArray.forEach === 'function') {
+    datesArray = dirtyDatesArray
+
+    // If `dirtyDatesArray` is Array-like Object, convert to Array. Otherwise, make it empty Array
+  } else {
+    datesArray = Array.prototype.slice.call(dirtyDatesArray)
+  }
+
+  var result
+  var minDistance
+  datesArray.forEach(function (dirtyDate, index) {
+    var currentDate = toDate(dirtyDate)
+
+    if (isNaN(currentDate)) {
+      result = NaN
+      minDistance = NaN
+      return
+    }
+
+    var distance = Math.abs(timeToCompare - currentDate.getTime())
+    if (result == null || distance < minDistance) {
+      result = index
+      minDistance = distance
+    }
+  })
+
+  return result
+}
---
+++ date-fns/libs/constants.ts
@@ -0,0 +1,3 @@
+export var maxTime = Math.pow(10, 8) * 24 * 60 * 60 * 1000
+
+export var minTime = -maxTime
