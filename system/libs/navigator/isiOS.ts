3c9d0dfdf153ecc3d4c8a583846eec32eaf1c10e
maxiaoqu <maxiaoqu@163.com>
2021-01-06 14:59:36 +0800

    删除function文件剩余的

--- system/libs/navigator/isiOS.ts
+++
@@ -1,8 +0,0 @@
-import {getUserAgent} from "../../utils/getNavigator";
-
-// ios终端
-export default function isIOS(): Boolean {
-  let iOS = !!getUserAgent().match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
-  return iOS
-}
-
