a8b17a586309502380cb44e8fc92eb2add62d65a
maxiaoqu <maxiaoqu@163.com>
2021-01-10 10:01:20 +0800

    修复Session

--- cache/libs/session/index.ts
+++ cache/libs/session/index.ts
@@ -1,7 +1,16 @@
-import session from "./sessionInfo";
+import isSession from "@idear-tools/judge";
+import session from "@idear-tools/config";
+
+const isCheck = (): boolean => {
+  let isHas: boolean = isSession()
+  if (!isSession()) console.error('对不起，您的运行环境不支持sessionStorage！')
+  return isHas
+}
+
 
 // 保存数据（可设置过期时间）
 export function setSession(sessionKey: string, sessionData: any, expires?: number) {
+  if (!isCheck()) return
   if (expires) {
     let params = {sessionKey, sessionData, expires}
     // 记录何时将值存入缓存，毫秒级
@@ -14,25 +23,30 @@ export function setSession(sessionKey: string, sessionData: any, expires?: numbe
 
 // 读取数据
 export function getSession(sessionKey: string) {
+  if (!isCheck()) return
   return JSON.parse(session.getItem(sessionKey))
 }
 
 // 删除数据
 export function removeSession(sessionKey: string) {
+  if (!isCheck()) return
   session.removeItem(sessionKey)
 }
 
 // 删除所有缓存数据
 export function cleanSession() {
+  if (!isCheck()) return
   session.clear()
 }
 
 // 得到某个索引的key
 export function sessionKey(keyIndex: number) {
+  if (!isCheck()) return
   return session.key(keyIndex)
 }
 
 // 得到session的缓存数量
 export function sessionNum() {
+  if (!isCheck()) return
   return session.length
 }
--- cache/utils/isWebSQL.ts
+++
@@ -1,8 +0,0 @@
-// 判断是否支持WebSQL
-// declare var openDatabase: any
-export default function isWebSQL() {
-  // @ts-ignore
-  let openDatabaseFun = openDatabase || (window as any).openDatabase
-  return typeof openDatabaseFun === 'function';
-}
-
