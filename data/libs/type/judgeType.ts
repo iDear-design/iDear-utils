8eee437b79758c2b2ebf9cf1e44e7ac1226074eb
maxiaoqu <maxiaoqu@163.com>
2021-01-12 09:46:05 +0800

    删除type文件并迁移到对应位置～

--- data/libs/type/judgeType.ts
+++
@@ -1,9 +0,0 @@
-/**
- * ## 是否字符串
- * @param {any} data
- * @param {String} type // String
- * @returns {boolean}
- */
-function isString(data: any, type: string): boolean {
-  return Object.prototype.toString.call(data) === `[object ${type}]`
-}
