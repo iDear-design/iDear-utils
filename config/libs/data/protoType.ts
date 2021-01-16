8fcf723dbdce46fa431a3137d343b87181f56d72
maxiaoqu <maxiaoqu@163.com>
2021-01-16 10:07:29 +0800

    修复config

--- config/libs/data/protoType.ts
+++ config/libs/data/protoType.ts
@@ -1,19 +1,17 @@
 /**
  * ## 是否字符串
  * @param {any} data 目标值
- * @returns {boolean}
+ * @returns {string} 'String'
  */
 export function protoTypeString(data: any): string {
-  // Object.prototype.toString.call(o).slice(8, -1) === 'String'
   return Object.prototype.toString.call(data).slice(8, -1)
 }
 
 /**
  * ## 是否字符串
  * @param {any} data 目标值
- * @returns {boolean}
+ * @returns {string} '[object String]'
  */
 export function protoTypeArr(data: any): string {
-  // Object.prototype.toString.call(str) === '[object String]'
   return Object.prototype.toString.call(data)
 }
