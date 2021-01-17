220bcedddfff43c671be4a08689cdd52fe10fca1
maxiaoqu <maxiaoqu@163.com>
2021-01-17 11:26:56 +0800

    合并重复的功能！

--- obtain/libs/screen/index.ts
+++ obtain/libs/screen/index.ts
@@ -1,3 +1,29 @@
-const screenInfo = screen || (window as any).screen
+import {win} from "@idear-tools/config"
+
+const screenInfo = screen || win.screen
+
+// 返回显示器屏幕的宽度
+export const width = screenInfo.width
+
+// 返回显示屏幕的高度
+export const height = screenInfo.height
+
+// 返回显示屏幕的宽度 (除 Windows 任务栏之外)
+export const availWidth = screenInfo.availWidth
+
+// 返回显示屏幕的高度 (除 Windows 任务栏之外)
+export const availHeight = screenInfo.availHeight
+
+// 任务栏宽度
+export const taskbarWidth = width - availWidth
+
+// 任务栏高度
+export const taskbarHeight = height - availHeight
+
+// 返回目标设备或缓冲器上的调色板的比特深度
+export const colorDepth = screenInfo.colorDepth
+
+// 返回显示屏幕的颜色分辨率（比特每像素
+export const pixelDepth = screenInfo.pixelDepth
 
 export default screenInfo
