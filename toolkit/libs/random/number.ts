7aee3829eee6d769331c383f4bfef49f8653346f
maxiaoqu <maxiaoqu@163.com>
2021-01-24 14:40:24 +0800

    random

---
+++ toolkit/libs/random/number.ts
@@ -0,0 +1,9 @@
+/**
+ * @desc 生成指定范围[min, max]的随机数
+ * @param  {Number} min
+ * @param  {Number} max
+ * @return {Number}
+ */
+export const randomNum = (min, max): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
