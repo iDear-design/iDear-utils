2c576292bf58a95c7e904080010d68c649de5e3f
maxiaoqu <maxiaoqu@163.com>
2021-02-05 11:34:49 +0800

    修复config

---
+++ src/config/mobileType.ts
@@ -0,0 +1,6 @@
+/**
+ * @desc 常见的手机类型
+ * */
+const mobileTypeData: string[] = ["android", "iphone", "symbianos", "windows phone", "ipad", "ipod"]
+
+export default mobileTypeData
--- src/config/userAgent.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @desc 常见的手机类型
- * */
-const userAgents: string[] = ["android", "iphone", "symbianos", "windows phone", "ipad", "ipod"]
-
-export default userAgents
