74f4595b812b2e6754b0a36eecbed6f1331375d9
maxiaoqu <maxiaoqu@163.com>
2021-02-09 13:25:04 +0800

    Scroll

--- src/element/scroll.ts
+++ src/element/scroll.ts
@@ -1,4 +1,4 @@
-import {doc} from "../config/system";
+import {doc, win} from "../config/system";
 
 /**
  * @desc 获取滚动条距顶部的距离
@@ -8,7 +8,41 @@ export const getScrollTop = (): number => {
 }
 
 /**
- * @desc  获取一个元素的距离文档(document)的位置，类似jQ中的offset()
+ * @desc 设置滚动条距顶部的距离
+ * @param {Number} to 滚动的距离
+ */
+export const setScrollTop = (to: number) => {
+  win.scrollTo(0, to);
+}
+
+/**
+ * @desc 在${duration}时间内，滚动条平滑滚动到${to}指定位置
+ * @param {Number} to 滚动的距离
+ * @param {Number} duration 滚动的时间
+ */
+export const scrollTo = (to: number, duration: number = 0) => {
+  if (duration < 0) {
+    setScrollTop(to);
+    return
+  }
+  var diff = to - getScrollTop();
+  if (diff === 0) return
+  var step = diff / duration * 10;
+  requestAnimationFrame(() => {
+    if (Math.abs(step) > Math.abs(diff)) {
+      setScrollTop(getScrollTop() + diff);
+      return;
+    }
+    setScrollTop(getScrollTop() + step);
+    if (diff > 0 && getScrollTop() >= to || diff < 0 && getScrollTop() <= to) {
+      return;
+    }
+    scrollTo(to, duration - 16);
+  });
+}
+
+/**
+ * @desc 获取一个元素的距离文档(document)的位置，类似jQ中的offset()
  * @param {HTMLElement} ele
  * @returns { {left: number, top: number} }
  */
