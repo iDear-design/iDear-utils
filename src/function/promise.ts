7fe6d5c7bd7fb7e33d61cfc6c197436b0d1cc5a6
maxiaoqu <maxiaoqu@163.com>
2021-02-10 12:03:49 +0800

    Promise

--- src/function/promise.ts
+++ src/function/promise.ts
@@ -1,27 +1,40 @@
-const PENDING = 'pending' // 初始未确定的状态
-const RESOLVED = 'resolved' // 成功的状态
-const REJECTED = 'rejected' // 失败的状态
+/**
+ * @desc 初始未确定的状态
+ */
+const PENDING = 'pending'
 
-/*
-Promise构造函数
-*/
-function Promise(excutor) {
+/**
+ * @desc 成功的状态
+ */
+const RESOLVED = 'resolved'
 
-  const self = this // Promise的实例对象
-  self.status = PENDING // 状态属性, 初始值为pending, 代表初始未确定的状态
-  self.data = undefined // 用来存储结果数据的属性, 初始值为undefined
-  self.callbacks = [] // {onResolved(){}, onRejected(){}}
+/**
+ * @desc 失败的状态
+ */
+const REJECTED = 'rejected'
 
-  /*
-  将promise的状态改为成功, 指定成功的value
-  */
+/**
+ * @desc Promise构造函数
+ * @param {String} excutor  订阅消息名称
+ */
+function Promise(excutor: any) {
+  // Promise的实例对象
+  const self = this
+  // 状态属性, 初始值为pending, 代表初始未确定的状态
+  self.status = PENDING
+  // 用来存储结果数据的属性, 初始值为undefined
+  self.data = undefined
+  // {onResolved(){}, onRejected(){}}
+  self.callbacks = []
+
+  /** 将promise的状态改为成功, 指定成功的value */
   function resolve(value) {
     // 如果当前不是pending, 直接结束
     if (self.status !== PENDING) return
-
-    self.status = RESOLVED // 将状态改为成功
-    self.data = value // 保存成功的value
-
+    // 将状态改为成功
+    self.status = RESOLVED
+    // 保存成功的value
+    self.data = value
     // 异步调用所有缓存的待执行成功的回调函数
     if (self.callbacks.length > 0) {
       // 启动一个延迟时间为0的定时器, 在定时器的回调中执行所有成功的回调
@@ -33,9 +46,7 @@ function Promise(excutor) {
     }
   }
 
-  /*
-  将promise的状态改为失败, 指定失败的reason
-  */
+  /** 将promise的状态改为失败, 指定失败的reason */
   function reject(reason) {
     // 如果当前不是pending, 直接结束
     if (self.status !== PENDING) return
@@ -54,54 +65,60 @@ function Promise(excutor) {
     }
   }
 
-  // 调用excutor来启动异步任务
+  /** 调用excutor来启动异步任务 */
   try {
     excutor(resolve, reject)
-  } catch (error) { // 执行器执行出错, 当前promise变为失败
-    console.log('-----')
+  } catch (error) {
+    // 执行器执行出错, 当前promise变为失败
+    console.error('ERROR：' + error)
     reject(error)
   }
 
 }
 
-/*
-用来指定成功/失败回调函数的方法
-    1). 如果当前promise是resolved, 异步执行成功的回调函数onResolved
-    2). 如果当前promise是rejected, 异步执行成功的回调函数onRejected
-    3). 如果当前promise是pending, 保存回调函数
-返回一个新的promise对象
-    它的结果状态由onResolved或者onRejected执行的结果决定
-    2.1). 抛出error ==> 变为rejected, 结果值为error
-    2.2). 返回值不是promise   ==> 变为resolved, 结果值为返回值
-    2.3). 返回值是promise    ===> 由这个promise的决定新的promise的结果(成功/失败)
-*/
+/**
+ * @desc 用来指定成功/失败回调函数的方法
+ *    1). 如果当前promise是resolved, 异步执行成功的回调函数onResolved
+ *    2). 如果当前promise是rejected, 异步执行成功的回调函数onRejected
+ *    3). 如果当前promise是pending, 保存回调函数
+ *    返回一个新的promise对象,它的结果状态由onResolved或者onRejected执行的结果决定
+ *        2.1). 抛出error ==> 变为rejected, 结果值为error
+ *        2.2). 返回值不是promise   ==> 变为resolved, 结果值为返回值
+ *        2.3). 返回值是promise    ===> 由这个promise的决定新的promise的结果(成功/失败)
+ * @param {Function} onResolved
+ * @param {Function} onRejected
+ */
 Promise.prototype.then = function (onResolved, onRejected) {
   const self = this
-
+  // 将reason向下传递
   onResolved = typeof onResolved === 'function' ? onResolved : value => value // 将value向下传递
   onRejected = typeof onRejected === 'function' ? onRejected : reason => {
     throw reason
-  } // 将reason向下传递
-
-  return new Promise((resolve, reject) => { // 什么时候改变它的状态
-
-    /*
-    1. 调用指定的回调函数
-    2. 根据回调执行结果来更新返回promise的状态
-    */
+  }
+  // 什么时候改变它的状态
+  return new Promise((resolve, reject) => {
+    /**
+     * 1. 调用指定的回调函数
+     * 2. 根据回调执行结果来更新返回promise的状态
+     */
     function handle(callback) {
       try {
-        const result = callback(self.data)
-        if (!(result instanceof Promise)) { //  2.2). 返回值不是promise   ==> 变为resolved, 结果值为返回值
+        const result: any = callback(self.data)
+        //  2.2). 返回值不是promise   ==> 变为resolved, 结果值为返回值
+        if (!(result instanceof Promise)) {
           resolve(result)
-        } else { // 2.3). 返回值是promise    ===> 由这个promise的决定新的promise的结果(成功/失败)
+        }
+        // 2.3). 返回值是promise    ===> 由这个promise的决定新的promise的结果(成功/失败)
+        else {
+          // @ts-ignore
           result.then(
             value => resolve(value),
             reason => reject(reason)
           )
-          // result.then(resolve, reject)
         }
-      } catch (error) { // 2.1). 抛出error ==> 变为rejected, 结果值为error
+      }
+        // 2.1). 抛出error ==> 变为rejected, 结果值为error
+      catch (error) {
         reject(error)
       }
     }
@@ -116,10 +133,10 @@ Promise.prototype.then = function (onResolved, onRejected) {
       })
     } else { // PENDING
       self.callbacks.push({
-        onResolved(value) {
+        onResolved(_value) {
           handle(onResolved)
         },
-        onRejected(reason) {
+        onRejected(_reason) {
           handle(onRejected)
         }
       })
