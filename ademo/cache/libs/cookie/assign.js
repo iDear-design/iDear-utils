c644d95f83308d597cc1252a4e1929f0bb2b9fdd
maxiaoqu <maxiaoqu@163.com>
2021-02-12 16:16:26 +0800

    删除对应的cache

--- ademo/cache/libs/cookie/assign.js
+++
@@ -1,9 +0,0 @@
-export default function (target) {
-  for (let i = 1; i < arguments.length; i++) {
-    let source = arguments[i]
-    for (let key in source) {
-      target[key] = source[key]
-    }
-  }
-  return target
-}
