bceaebb4de546ee7fd7baed1fd176e46725f4fbf
maxiaoqu <maxiaoqu@163.com>
2021-02-14 13:27:36 +0800

    url参数转对象\url对象序列化

---
+++ src/check/urlQuery.ts
@@ -0,0 +1,33 @@
+/**
+ * @desc url参数转对象
+ * @param {string} url 地址，默认：window.location.href
+ * @returns {object}
+ */
+export const getQuery = (url: string): object => {
+  url = url == null ? window.location.href : url
+  let search = url.substring(url.lastIndexOf('?') + 1)
+  if (!search) return {}
+  let query: object = JSON.parse('{"' + decodeURIComponent(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}') || {}
+  return query
+}
+
+/**
+ * @desc url对象序列化
+ * @param {object} url 地址
+ * @returns {string}
+ */
+export const setQuery = (obj: object): string => {
+  if (!obj) return '';
+  let pairs = [];
+  for (let key in obj) {
+    let value = obj[key];
+    if (value instanceof Array) {
+      for (let i = 0; i < value.length; ++i) {
+        pairs.push(encodeURIComponent(key + '[' + i + ']') + '=' + encodeURIComponent(value[i]));
+      }
+      continue;
+    }
+    pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+  }
+  return pairs.join('&');
+}
