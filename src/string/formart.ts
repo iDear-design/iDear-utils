46bf66474ead2735c4b312708a65155dfbbcbe49
maxiaoqu <maxiaoqu@163.com>
2021-02-15 14:13:19 +0800

    字符串的格式化

--- src/string/formart.ts
+++ src/string/formart.ts
@@ -1,13 +1,36 @@
 /** @desc 字符串的格式化 */
-import {changeCaseConfig} from "../../types/numType";
+import {changeCaseConfig, trimStringConfig} from "../../types/numType";
+
+/**
+ * ## 去除空格
+ * @param {string} str 目标字符串
+ * @param {number} type 1-所有空格  2-前后空格  3-前空格 4-后空格
+ * @returns {string}
+ */
+
+export const strTrim = (str: string, type?: trimStringConfig): string => {
+  type = type || 1;
+  switch (type) {
+    case 1:
+      return str.replace(/\s+/g, '');
+    case 2:
+      return str.replace(/(^\s*)|(\s*$)/g, '');
+    case 3:
+      return str.replace(/(^\s*)/g, '');
+    case 4:
+      return str.replace(/(\s*$)/g, '');
+    default:
+      return str;
+  }
+}
 
 /**
  * @desc 提还字母大小写
- * @param  {String} str
- * @param  {Number} type 1-首字母大写  2-首页母小写  3-大小写转换  4-全部大写  5-全部小写
- * @returns {number}
+ * @param  {string} str 目标字符串
+ * @param  {number} type 1-首字母大写  2-首页母小写  3-大小写转换  4-全部大写  5-全部小写
+ * @returns {string}
  */
-export const changeCase = (str: string, type: changeCaseConfig): string => {
+export const strChangeCase = (str: string, type: changeCaseConfig): string => {
   switch (type) {
     case 1:
       return str.replace(/\b\w+\b/g, function (word) {
@@ -36,4 +59,35 @@ export const changeCase = (str: string, type: changeCaseConfig): string => {
     default:
       return str;
   }
+}
+
+/**
+ * @desc 生成一个倒序的字符串
+ * @param  {string} str 目标字符串
+ * @returns {string}
+ */
+export const strReverse = (str: string): string => {
+  // return str.split('').reverse().join('')
+  // return [...str].reverse().join('')
+  return Array.from(str).reverse().join('')
+}
+
+/**
+ * @desc 字符串是否是回文: 如果给定的字符串是回文，则返回 true ；否则返回 false
+ * @param  {string} str 目标字符串
+ * @returns {boolean}
+ */
+export const strPalindrome = (str: string): boolean => {
+  return str === strReverse(str)
+}
+
+/**
+ * @desc 截取字符串: 如果字符串的长度超过了num, 截取前面num长度部分, 并以...结束
+ * @param  {string} str 目标字符串
+ * @param  {number} num 字符串限定长度
+ * @param  {string} truncate 超出的展示形式
+ * @returns {boolean}
+ */
+export const strTruncate = (str: string, num: number, truncate: string = '...'): string => {
+  return str.length > num ? str.slice(0, num) + truncate : str
 }
