459594d6903e017a9ec6342c7455f2ee62732b21
maxiaoqu <maxiaoqu@163.com>
2021-05-18 20:13:40 +0800

    移动config位置

---
+++ libs/config/dataType.ts
@@ -0,0 +1,17 @@
+/**
+ * @description 判断类型
+ * @param {any} data 目标值
+ * @returns {string} 'String'
+ */
+export function dataTypeString(data: any): string {
+  return Object.prototype.toString.call(data).slice(8, -1)
+}
+
+/**
+ * @description 判断类型
+ * @param {any} data 目标值
+ * @returns {string} '[object String]'
+ */
+export function dataTypeArr(data: any): string {
+  return Object.prototype.toString.call(data)
+}
---
+++ libs/config/system.ts
@@ -0,0 +1,14 @@
+/**
+ * @description 导出window
+ * */
+export const win: Window | any = window || (window as any)
+
+/**
+ * @description 导出document
+ * */
+export const doc: Document | any = document || (document as any)
+
+/**
+ * @description 导出Navigator
+ * */
+export const nav: Navigator | any = navigator || (navigator as any)
