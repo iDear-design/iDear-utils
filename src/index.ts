334c56dd1465599489fe487c8e57057af51d9af5
maxiaoqu <maxiaoqu@163.com>
2021-05-18 19:38:44 +0800

    为导出做准备～

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
+/** array---数组 */
 
+/** cache---缓存 */
+
+/** check---获取、检查 */
+
+/** config---基础配置 */
+
+/** create---创建 */
+
+/** download---下载、导出 */
+
+/** element---element相关 */
+
+/** files---文件相关 */
+
+/** format---相关的格式化 */
+
+/** function---函数、方法 */
+
+/** judge---判断 */
+
+/** listener---监听事件 */
+
+/** notify---提示、弹窗 */
+
+/** number---数字 */
+
+/** object---对象 */
+
+/** systemOS---系统、环境相关的 */
