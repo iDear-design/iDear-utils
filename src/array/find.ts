29594942da658f11b55666fc35a7403699d7416c
maxiaoqu <maxiaoqu@163.com>
2021-05-23 11:35:35 +0800

    获取数组内指定值出现的次数

--- src/array/find.ts
+++ src/array/find.ts
@@ -43,3 +43,17 @@ export const arrLastIndexOf = (arr: Array<any>, val: any): number => {
 export const arrFindMaxOrMin = (arr: Array<number>, type: string = 'max'): number => {
   return Math[type].apply(null, arr);
 }
+
+/**
+ * @description 获取数组内指定值出现的次数
+ * @param {Array<number | string>} arr---目标数组
+ * @param {string} type---最大：max、最小: min
+ * @param val 值
+ */
+export const arrCount = (arr: Array<number | string>, value: number | string): number => {
+  let count: number = 0
+  for (let len = arr.length - 1; len >= 0; len--) {
+    if (value === arr[len]) count + 1
+  }
+  return count
+}
