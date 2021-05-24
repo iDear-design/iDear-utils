eb72c563e5645fe1e67001189c258d5e5877b882
maxiaoqu <maxiaoqu@163.com>
2021-05-24 10:19:40 +0800

    判断是否为空

--- src/judge/dataEmpty.ts
+++ src/judge/dataEmpty.ts
@@ -1,9 +1,35 @@
+import {dataTypeString} from "../_base/dataType";
+
+/**
+ * @description 判断是否为空
+ * @param {any} target
+ * @return {Boolean}
+ */
+export const isEmpty = (target: any): boolean => {
+  let empty: boolean = false;
+  if (dataTypeString(target) === 'Object') return isObjEmpty(target)
+  if (dataTypeString(target) === 'String') return isStrEmpty(target)
+  if (dataTypeString(target) === 'Array') return isArrEmpty(target)
+  if (target === null || target === undefined || target === NaN) empty = true
+  return empty
+}
+
+
+/**
+ * @description 判断`str`是否为空
+ * @param {string} str
+ * @return {Boolean}
+ */
+export const isStrEmpty = (str: string): boolean => {
+  return str.length === 0
+}
+
 /**
  * @description 判断`obj`是否为空
  * @param {Object} obj
  * @return {Boolean}
  */
-export const isObjEmpty = (obj: Object): boolean => {
+export const isObjEmpty = (obj: object): boolean => {
   return Object.keys(obj).length === 0
 }
 
